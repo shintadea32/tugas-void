2f7f66a3538f29636425a0d09e65dcaf36b575c3
shintadewi29rpl <shinta_dewi_29rpl@student.smktelkom-mlg.sch.id>
2021-03-08 21:04:17 +0700

    tugas void

---
+++ src/belajarrrr/halo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package belajarrrr;
+
+/**
+ *
+ * @author I d e a P a d
+ */
+public class halo {
+     String kenal = "";
+    
+    void New(String kenal){
+        this.kenal=kenal;
+    }
+    void hasil(){
+        System.out.println(kenal);
+}
+}
---
+++ src/belajarrrr/say.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package belajarrrr;
+
+/**
+ *
+ * @author I d e a P a d
+ */
+public class say {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        halo objek = new halo();
+        objek.New("Haloooo");
+        objek.hasil();
+    }
+    
+}
